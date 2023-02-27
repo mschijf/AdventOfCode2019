57a498614e67e16dedd8885f09e5f52176336126
mschijf <mschijf@gmail.com>
2023-02-27 20:36:40 +0100

    December 21 - Part 1,

---
+++ src/main/kotlin/adventofcode2019/december21/PuzzleSolver.kt
@@ -0,0 +1,69 @@
+package adventofcode2019.december21
+
+import adventofcode2019.Input
+import adventofcode2019.IntCodeProgramCR
+import adventofcode2019.PuzzleSolverAbstract
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    PuzzleSolver(test=false).showResult()
+}
+
+class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
+
+    override fun resultPartOne(): String {
+        val options = listOf(0,1,2,3,4,12,13,14,23,24,34,123,124,134,234,1234)
+        options.forEach {optionNumber ->
+            val springdroid = Springdroid(inputLines.first(), optionNumber)
+            val result = springdroid.walk()
+            if (result >= 0) {
+                println("program $optionNumber reaches the end! It returns $result")
+            }
+        }
+
+        return "^^^^^"
+    }
+}
+
+class Springdroid(inputLine: String, private val programNumber: Int) {
+
+    private val intCodeProgram = IntCodeProgramCR( inputLine.split(",").map { it.toLong() } )
+    var result: Long = -1
+
+    fun walk(): Long {
+        runBlocking {
+            launch {
+                intCodeProgram.runProgram()
+            }
+            runProgram()
+            printOutput()
+        }
+        return result
+    }
+
+    private suspend fun runProgram() {
+        val program = Input("data/december21/", "droid_program$programNumber").inputLines
+        program.filter{it.isNotEmpty() && !it.startsWith("#")}.forEach { line -> sendInput(line) }
+    }
+
+    private suspend fun sendInput(s: String) {
+        s.forEach { ch -> intCodeProgram.input.send(ch.code.toLong()) }
+        intCodeProgram.input.send(10L)
+    }
+
+    private suspend fun printOutput() {
+        while (!intCodeProgram.output.isClosedForReceive) {
+            val output = intCodeProgram.output.receive()
+            if (output > 255) {
+                //println(output)
+                result = output
+            } else {
+                //print(output.toInt().toChar())
+            }
+        }
+    }
+
+
+}
+
